22e6326be4a87728eadf1cff77ca4ebd50ec94c9
thenori-o <the_norio@outlook.com>
2024-04-22 21:50:11 -0300

    feat: create feedback route and configure json response

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,25 @@
 import express from 'express';
-
-const app = express();
-app.use('/', async (req, res) => {
-  res.send('Hello World');
-});
+import { prisma } from './prisma';
 
 const PORT = process.env.PORT || 3333;
+
+const app = express();
+app.use(express.json());
+
+app.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot
+    }
+  })
+
+  return res.status(201).json({ data: feedback });
+});
+
 app.listen(PORT, () => {
   console.log('HTTP server running on port', PORT);
 });
