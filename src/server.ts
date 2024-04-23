b1233a76300fe5c1d95caa3cfdc6b1841a643c3f
thenori-o <the_norio@outlook.com>
2024-04-22 22:03:50 -0300

    feat: config mail sending

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
 import { prisma } from './prisma';
 
 const PORT = process.env.PORT || 3333;
 
 const app = express();
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "2bae59296ab790",
+    pass: "658839f314b826"
+  }
+});
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -15,6 +25,18 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot
     }
+  });
+
+  await transport.sendMail({
+    from: 'Feedget team <noreply.@feedget.com>',
+    to: 'Lucas Tenorio <lucas@outlook.com>',
+    subject: 'New feedback',
+    html: [
+      `<div style='font-family: sans-serif; font-size: 16px; color: #111'>`,
+      `<p>Feedback type: ${type}</p>`,
+      `<p>Comment: ${comment}</p>`,
+      `</div>`,
+    ].join('\n')
   })
 
   return res.status(201).json({ data: feedback });
